c13481d06837ce3c8e85c5280cb7b96cddb00c42
Abhishek-Punhani <punhani.manavabhi@gmail.com>
2025-10-19 02:30:06 +0530

    some changes
    Signed-off-by: Abhishek-Punhani <punhani.manavabhi@gmail.com>

---
+++ apps/web/pages/activate/[...token].tsx
@@ -0,0 +1,66 @@
+"use client";
+import axios from "axios";
+import { useRouter, useParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import { useToast } from "@/hooks/use-toast";
+
+export default function ActivateEmail() {
+  const router = useRouter();
+  const params = useParams();
+  const token = Array.isArray(params.token)
+    ? params.token.join("")
+    : params.token;
+  const [loading, setLoading] = useState(true);
+
+  const { toast } = useToast();
+
+  useEffect(() => {
+    if (token) {
+      async function verifyToken() {
+        try {
+          const res = await axios.post(
+            `${process.env.NEXT_PUBLIC_AUTH_SERVER}/api/auth/verify-email`,
+            { token }
+          );
+          if (res.status === 200) {
+            toast({
+              title: "Email Verified",
+              description: "You can now login to your account.",
+            });
+          } else {
+            throw new Error("Verification failed");
+          }
+        } catch (error: any) {
+          toast({
+            title: "Error",
+            description:
+              error?.response?.data?.message || "Failed to verify email.",
+            variant: "destructive",
+          });
+        } finally {
+          setLoading(false);
+        }
+      }
+      verifyToken();
+    }
+  }, [token, toast]);
+
+  useEffect(() => {
+    if (!loading) {
+      router.replace("/auth/login");
+    }
+  }, [loading, router]);
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "60vh",
+      }}
+    >
+      Loading ...
+    </div>
+  );
+}
